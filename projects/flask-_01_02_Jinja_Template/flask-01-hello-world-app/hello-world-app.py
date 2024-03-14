abc87dd0334c3d5d89beda370a6b288446afef86
Aynuur <anurbostanci@gmail.com>
2024-03-14 11:09:42 +0100

    ilk dosyalar olustu

--- projects/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ projects/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,8 +1,19 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/') #ana sayfayi olusturuyoruz. dekorasyon satirlari denir app route satirlarina
+def head(): #ana sayfaya ne istiyorsak buraya ekliyoruz
+     return 'Hello word Aynur'
 
-
+@app.route('/secondpage') #ikinci sayfayi ekliyoruz
+def second():
+     return 'This is second World'
+@app.route('/third')
+def third():
+     return 'This is third page'
+@app.route('/fourth/<string:id>')
+def fourth(id):
+     return f'Id of this page is {id}'
 
 
 
@@ -12,5 +23,5 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
 
-     app.run(debug=True, port=3000)
-     # app.run(host= '0.0.0.0', port=8081)
+     #app.run(debug=True) #hatalari görmek istiyorsak debug i  aktif birakacaz
+      app.run(host= '0.0.0.0', port=80) #farkli port istersek onu yazabilirz port bildirmessen 500 portunda calisir
